261f9f0bccfaddf59d275a15fd1357c6e238e264
Jasmyre <lanuzajasmyreandrei@gmail.com>
2024-12-23 12:21:44 +0800

    Feat: Added hero section effects

--- components/home/hero-section/HeroSection.tsx
+++ components/home/hero-section/HeroSection.tsx
@@ -3,6 +3,7 @@ import { TypoH1, TypoP } from "@/components/Typography";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Animated from "../../template/Animated";
+import { BackgroundLines } from "@/components/ui/background-lines";
 
 export const HeroSectionBlissBrew = () => {
   return (
@@ -29,46 +30,54 @@ export const HeroSectionBlissBrew = () => {
 const HeroSection = () => {
   return (
     <section id="Hero_section" className="scroll-m-20 border-b bg-box">
-      <div className="absolute bottom-[25%] left-0 right-0 top-0 -z-10 m-auto hidden h-[310px] w-[310px] rounded-full bg-primary/50 opacity-20 blur-[100px] dark:block"></div>
-      <div className="absolute bottom-[25%] right-0 top-0 -z-10 m-auto hidden h-[310px] w-[310px] rounded-full bg-primary/50 opacity-20 blur-[100px] dark:block max-md:hidden"></div>
-      <div className="absolute bottom-0 left-0 top-[25%] -z-10 m-auto hidden h-[310px] w-[310px] rounded-full bg-primary/50 opacity-20 blur-[100px] dark:block max-md:hidden"></div>
+      <BackgroundLines className="bg-transparent dark:bg-transparent">
+        <div className="absolute bottom-[25%] left-0 right-0 top-0 -z-10 m-auto hidden h-[310px] w-[310px] rounded-full bg-primary/50 opacity-20 blur-[100px] dark:block"></div>
+        <div className="absolute bottom-[25%] right-0 top-0 -z-10 m-auto hidden h-[310px] w-[310px] rounded-full bg-primary/50 opacity-20 blur-[100px] dark:block max-md:hidden"></div>
+        <div className="absolute bottom-0 left-0 top-[25%] -z-10 m-auto hidden h-[310px] w-[310px] rounded-full bg-primary/50 opacity-20 blur-[100px] dark:block max-md:hidden"></div>
 
-      <div
-        id="hero_section_wrapper"
-        className="container flex min-h-[85vh] flex-col items-center justify-center gap-10 text-center section"
-      >
-        <div className="flex flex-col gap-2">
-          <Animated>
-            <TypoH1>
-              Minimalist <strong className="text-primary">Web Designer</strong>&{" "}
-              <strong className="text-primary">Programmer</strong>
-            </TypoH1>
-          </Animated>
+        <div
+          id="hero_section_wrapper"
+          className="container flex min-h-[85vh] flex-col items-center justify-center gap-10 text-center section"
+        >
+          <div className="flex flex-col gap-2">
+            <Animated>
+              <TypoH1>
+                Minimalist{" "}
+                <strong className="text-primary">Web Designer</strong>&{" "}
+                <strong className="text-primary">Programmer</strong>
+              </TypoH1>
+            </Animated>
 
-          <Animated>
-            <TypoP className="text-secondary-foreground">
-              I create simple, clean, and efficient web solutions.
-            </TypoP>
-          </Animated>
+            <Animated>
+              <TypoP className="text-secondary-foreground">
+                I create simple, clean, and efficient web solutions.
+              </TypoP>
+            </Animated>
+          </div>
+          <div className="flex flex-wrap items-center justify-center gap-4">
+            <Animated>
+              <Button
+                variant={"outline"}
+                className="flex-1 bg-transparent"
+                size={"lg"}
+              >
+                Download Resume
+              </Button>
+            </Animated>
+
+            <Animated>
+              <Button
+                asChild
+                variant={"default"}
+                className="flex-1"
+                size={"lg"}
+              >
+                <Link href={"#portfolio"}>View Portfolio</Link>
+              </Button>
+            </Animated>
+          </div>
         </div>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-          <Animated>
-            <Button
-              variant={"outline"}
-              className="flex-1 bg-transparent"
-              size={"lg"}
-            >
-              Download Resume
-            </Button>
-          </Animated>
-
-          <Animated>
-            <Button asChild variant={"default"} className="flex-1" size={"lg"}>
-              <Link href={"#portfolio"}>View Portfolio</Link>
-            </Button>
-          </Animated>
-        </div>
-      </div>
+      </BackgroundLines>
     </section>
   );
 };
